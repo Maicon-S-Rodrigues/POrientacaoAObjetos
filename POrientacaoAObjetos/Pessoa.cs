7b04598fed6942fcac44c558a113a869b015a52d
Maicon Rodrigues <maicon.rodrigues2010mr@gmail.com>
2022-08-23 16:02:41 -0300

    terminado anotações sobre os exempos com objetos/classes/etc

---
+++ POrientacaoAObjetos/Pessoa.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POrientacaoAObjetos
+{
+    internal class Pessoa
+    {
+        //ATRIBUTOS DECLARADOS: (obs:Atributos de uma classe são praticamente dizendo "iguais" a variaveis
+        String nome;
+        int idade;
+        public String cidade; // modificou a restrição de acesso para público
+
+
+        ///classe/ métodos construtores: <summary>
+        ///métodos construtores SEMPRE tem o mesmo nome da classe, eles são executados quando o objeto é instânciado (criado)
+        public Pessoa()
+        {
+            this.nome = "Zé Ninguém";
+            this.idade = 0;
+            this.cidade = "Araraquara";
+        }
+
+        ///neste método construtor ele será executado na criação do objeto e irá receber os valores por parâmetros para preencher seus atributos
+        public Pessoa(string nome, int idade, string cidade)
+        {
+            this.nome=nome;
+            this.idade = idade;
+            this.cidade = cidade;
+        }
+
+        public override string ToString()
+        {
+            return "Nome : " + nome + "\nIdade: " + idade + "\nCidade: " + cidade;
+        }
+
+
+        public void setNome (String nome) /// Método SET para receber o nome
+        {
+           this.nome = nome;
+        }
+        public void setIdade (int idade) // Método SET para receber a idade
+        {
+            this.idade = idade;
+        }
+
+        public String getNome () // Método GET para retornar o valor do atributo nome
+        {
+            return nome;
+        }
+        public int getIdade () // Método GE para retornar o valor do atributo idade
+        {
+            return idade;
+        }
+    }
+}
--- POrientacaoAObjetos/Program.cs
+++ POrientacaoAObjetos/Program.cs
@@ -6,7 +6,22 @@ namespace POrientacaoAObjetos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pessoa pessoa = new Pessoa();
+            Pessoa pessoa1 = new Pessoa();
+            Pessoa p3;
+            //p3 = new Pessoa();
+            p3 = pessoa1;
+
+            Console.WriteLine("Nome (pessoa): " + pessoa.getNome());
+
+            pessoa.setNome("MARIA");
+            pessoa1.setNome("ANA MARIA");
+
+            Console.WriteLine("Novo nome (pessoa): " + pessoa1.getNome());
+            Console.WriteLine(p3.getNome());
+            
+
+
         }
     }
 }
